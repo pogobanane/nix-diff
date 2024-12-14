84fd5d54374d61f243300d030ec9d9dcd32d4b54
Pogobanane <aenderboy@gmx.de>
2024-12-14 01:03:36 +0100

    more stuff works

--- main.cc
+++ main.cc
@@ -1,4 +1,6 @@
+// #include "nix_api_expr_internal.h"
 #include <fstream>
+#include <iostream>
 #include <nix_api_util.h>
 #include <nix_api_expr.h>
 #include <nix_api_value.h>
@@ -21,13 +23,23 @@ int main()
     Store * store = nix_store_open(NULL, "dummy://", NULL);
     EvalState * state = nix_state_create(NULL, NULL, store); // empty search path (NIX_PATH)
     Value * value = nix_alloc_value(NULL, state);
+    // nix_printer nix_printer = nix_printer(std::cout);
 
     std::ifstream ifs("dump-config.nix");
     std::string content( (std::istreambuf_iterator<char>(ifs) ),
                        (std::istreambuf_iterator<char>()    ) );
 
+    // nix_expr_eval_from_string(NULL, state, "{ \"a\" = \"foo\"; }", ".", value);
     nix_expr_eval_from_string(NULL, state, content.c_str(), ".", value);
     nix_value_force(NULL, state, value);
+
+    printf("fine\n");
+
+    const char* typename_ = nix_get_typename(NULL, value);
+    printf("Type: %s\n", typename_);
+
+    // std::cout << "foo " <<  value->print(std::cout) << std::endl;
+
 
     char * version;
     nix_get_string(NULL, value, my_get_string_cb, &version);
